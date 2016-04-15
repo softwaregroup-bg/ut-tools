bca9af30117e9c588d1c082c56272277bb2540d1
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-04-15 13:38:34 +0300

    fix: add fixed output filename

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -3,7 +3,7 @@ re='([^/]*\/[^/]*)\.git$'
 if [[ $CI_BUILD_REPO =~ $re ]]
 then
   echo building ${BASH_REMATCH[1]}
-  wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile && \
+  wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
   docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
   docker run -i --rm ${BASH_REMATCH[1]} npm run gitlab
 else
