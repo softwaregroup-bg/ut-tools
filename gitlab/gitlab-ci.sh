e1474fef413ec08bada8350766b550e13350572a
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-04-22 15:26:07 +0300

    fix: fix params passing order

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -5,7 +5,7 @@ then
   echo building ${BASH_REMATCH[1]}
   wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
   docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
-  docker run -i --rm ${BASH_REMATCH[1]} -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} npm run gitlab
+  docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
 else
   echo invalid CI_BUILD_REPO $CI_BUILD_REPO
   exit 1
