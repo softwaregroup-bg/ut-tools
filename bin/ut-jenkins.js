905bd8948e6505c4b1e5641e14196cbe87865b7a
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-04-27 09:13:12 +0300

    fix: parse module from gitlab repo

--- bin/ut-jenkins.js
+++ bin/ut-jenkins.js
@@ -2,6 +2,9 @@
 /*eslint no-process-env:0*/
 require('../lib/setEnv');
 
+var module = (process.env.gitlabSourceRepoName || '').match(/^.*\/.*-(.*)$/);
+process.env.UT_MODULE = module && module[1];
+
 var command;
 var jobname = process.env.JOB_NAME || '';
 
