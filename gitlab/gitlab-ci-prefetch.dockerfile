a56080e70a52ffcb55a4dbf848b3368a48dfeb22
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-06-17 11:04:07 +0300

    feat: add support for prefetch.json

--- gitlab/gitlab-ci-prefetch.dockerfile
+++ gitlab/gitlab-ci-prefetch.dockerfile
@@ -2,8 +2,11 @@ FROM risingstack/alpine:3.4-v4.4.4-3.6.1
 RUN npm i -g npm@3
 
 COPY .npmrc .npmrc
-COPY package.json package.json
+COPY prefetch.json package.json
 RUN npm --production=false install
+
+COPY package.json package.json
+RUN npm --production=false update
 
 # Add your source files
 COPY . .
--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -3,7 +3,12 @@ re='([^/]*\/((impl-)|(ut-))([^/]*))\.git$'
 if [[ $CI_BUILD_REPO =~ $re ]]
 then
   echo building ${BASH_REMATCH[1]}
-  wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
+  if [ -f prefetch.json ]
+  then
+    wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci-prefetch.dockerfile -O gitlab-ci.dockerfile && \
+  else
+    wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
+  fi
   docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
   docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
 else
