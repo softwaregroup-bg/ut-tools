01f54a319066132a7ba268e807cda874e34c9a84
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-06-17 11:31:34 +0300

    fix: fix error

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -6,11 +6,13 @@ then
   if [ -f prefetch.json ]
   then
     wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci-prefetch.dockerfile -O gitlab-ci.dockerfile && \
+    docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
+    docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   else
     wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
+    docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
+    docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   fi
-  docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
-  docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
 else
   echo invalid CI_BUILD_REPO $CI_BUILD_REPO
   exit 1
