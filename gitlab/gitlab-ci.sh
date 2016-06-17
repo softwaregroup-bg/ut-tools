d7dcc93c08c6778dd8072ba0017680aca880e717
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-06-17 11:46:08 +0300

    feat: add cache volume

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -7,11 +7,11 @@ then
   then
     wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci-prefetch.dockerfile -O gitlab-ci.dockerfile && \
     docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
-    docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
+    docker run -i --rm -v /srv/npm:/root/.npm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   else
     wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
     docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
-    docker run -i --rm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
+    docker run -i --rm -v /srv/npm:/root/.npm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   fi
 else
   echo invalid CI_BUILD_REPO $CI_BUILD_REPO
