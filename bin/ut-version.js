7dbb161ce9115819bb0781a86e66a275d26d8562
Pavel Stefanov <pavkata889@gmail.com>
2016-11-17 12:20:56 +0200

    Create git tag if there none exists

--- bin/ut-version.js
+++ bin/ut-version.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var path = require('path');
 
-var gitTag = require('../lib/exec')('git', ['describe'], 'pipe');
+var gitTag = require('../lib/exec')('git', ['describe'], 'pipe', false) || require('../lib/exec')('git', ['tag', '-a', 'initial_tag', '-m', '"Initial tag"'], 'pipe');
 var pkgVersion = require(path.join(process.cwd(), 'package.json')).version;
 if (gitTag !== 'v' + pkgVersion) {
     require('../lib/exec')('npm', ['run', 'changelog', '--silent']);
