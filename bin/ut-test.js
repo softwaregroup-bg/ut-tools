e3e34bb6dd8c270e21b4f6d8d625fd3cbe0c2c47
Pavkata Stefanov <pavel.stefanov@softwaregroup-bg.com>
2017-01-13 17:05:12 +0200

    Add code coverage calculation

--- bin/ut-test.js
+++ bin/ut-test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-require('../lib/exec')('node', [
+require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
+    'node',	
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js']);
+    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'])
