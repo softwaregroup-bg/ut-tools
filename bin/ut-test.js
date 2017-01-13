650e1b1409fb928aa016104e5d6abbbe1cbeed8d
Pavel Stefanov <pavkata889@gmail.com>
2017-01-13 17:09:43 +0200

    Add semicollon

--- bin/ut-test.js
+++ bin/ut-test.js
@@ -4,4 +4,4 @@ require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
     'node',	
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'])
+    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js']);
