cb4da467f487d275571fba009e115075707aa907
Pavel Stefanov <pavkata889@gmail.com>
2017-01-24 15:13:01 +0200

    feat: turn on code coverage

--- bin/ut-cover.js
+++ bin/ut-cover.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
+    '--reporter=lcov',
+    '--reporter=text',
+    '--reporter=cobertura',
+    '--include=**/*.js',
+    '--exclude=!node_modules/ut-*/**/*.js',
+    '--exclude=node_modules/!(ut-*)/**/*.js',
+    '--exclude=**/test/**',
     'node',
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
