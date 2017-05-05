9d247936424b61c647e3ff9a2a3e7a4ceeb9a804
venetahristova <veneta.vl.hristova@gmail.com>
2017-05-05 15:27:20 +0300

    feat: allow passing arguments to ut-test (#29)

--- bin/ut-test.js
+++ bin/ut-test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+/* eslint no-process-env:0 */
 
 require('../lib/exec')('node', [
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js']);
+    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(process.argv.slice(2)));
