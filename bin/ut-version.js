5740f4ffe8fe08b2724fb45e67269182d96759f5
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-05-05 15:50:17 +0300

    fix: dont make changelog if pkg version is prerelease

--- bin/ut-version.js
+++ bin/ut-version.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 var path = require('path');
-
-var gitTag = require('../lib/exec')('git', ['describe'], 'pipe', false) || require('../lib/exec')('git', ['tag', '-a', 'initial_tag', '-m', '"Initial tag"'], 'pipe');
+var semver = require('semver');
 var pkgVersion = require(path.join(process.cwd(), 'package.json')).version;
-if (gitTag !== 'v' + pkgVersion) {
-    require('../lib/exec')('npm', ['run', 'changelog', '--silent']);
+
+if (!semver.prerelease(pkgVersion)) {
+    var gitTag = require('../lib/exec')('git', ['describe'], 'pipe', false) || require('../lib/exec')('git', ['tag', '-a', 'initial_tag', '-m', '"Initial tag"'], 'pipe');
+    if (gitTag !== 'v' + pkgVersion) {
+        require('../lib/exec')('npm', ['run', 'changelog', '--silent']);
+    }
 }
