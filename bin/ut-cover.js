82b9c86a6a1d864f5d2240bafdf41b197ec55a02
kalinkrustev <kalin.krustev@gmail.com>
2017-05-26 14:21:01 +0300

    * fix: prevent running cover twice for release branches and tests running twice on others

--- bin/ut-cover.js
+++ bin/ut-cover.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
-
+/* eslint no-process-env:0 */
 require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
     '--reporter=lcov',
     '--reporter=text',
     '--reporter=cobertura',
     '--include=**/*.js',
+    '--exclude=**/ut-tools/**/*.js',
     '--exclude=!node_modules/ut-*/**/*.js',
     '--exclude=node_modules/!(ut-*)/**/*.js',
     '--exclude=**/test/**',
     'node',
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js']);
+    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(
+        process.env.UT_COVER_DIR ? ['--', '--dir', process.env.UT_COVER_DIR] : []
+    ));
