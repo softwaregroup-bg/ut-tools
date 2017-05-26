580f9a761b357bfa9085e6e504b2c43b19655122
kalinkrustev <kalin.krustev@softwaregroup-bg.com>
2017-05-26 15:50:26 +0300

    fix: cover dir

--- bin/ut-cover.js
+++ bin/ut-cover.js
@@ -8,10 +8,13 @@ require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
     '--exclude=**/ut-tools/**/*.js',
     '--exclude=!node_modules/ut-*/**/*.js',
     '--exclude=node_modules/!(ut-*)/**/*.js',
-    '--exclude=**/test/**',
-    'node',
-    require('../lib/babelNodePath'),
-    require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(
-        process.env.UT_COVER_DIR ? ['--', '--dir', process.env.UT_COVER_DIR] : []
-    ));
+    '--exclude=**/test/**'].concat(
+        process.env.UT_COVER_DIR ? [`--report-dir=${process.env.UT_COVER_DIR}`] : []
+    ).concat([
+        'node',
+        require('../lib/babelNodePath'),
+        require.resolve('blue-tape/bin/blue-tape'),
+        'test/**/start*.js',
+        'test/**/test*.js',
+        'test/**/stop*.js'
+    ]));
