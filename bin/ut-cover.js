14dc381195db7b553eece13d9f6172152736dfa2
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-22 17:06:27 +0300

    fix: if jest is set up run cover on both integration and ui tests

--- bin/ut-cover.js
+++ bin/ut-cover.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 /* eslint no-process-env:0 */
+var fs = require('fs');
+
 require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
     '--reporter=lcov',
     '--reporter=text',
@@ -19,3 +21,13 @@ require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
         'test/**/test*.js',
         'test/**/stop*.js'
     ]));
+try {
+    require('../lib/exec')(require.resolve('jest/bin/jest'), [
+        '--coverage'
+    ].concat(
+        process.env.UT_COVER_DIR ? [`--coverageDirectory=${process.env.UT_COVER_DIR}_ui`] : []
+    ));
+    fs.appendFileSync(`${process.env.UT_COVER_DIR}/lcov.info`, fs.readFileSync(`${process.env.UT_COVER_DIR}_ui/lcov.info`));
+} catch (e) {
+    console.error('No ui tests')
+}
