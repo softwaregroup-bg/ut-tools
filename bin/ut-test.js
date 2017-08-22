f04684910e13d0e2cea78a5fe27db261304288c3
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-22 17:22:49 +0300

    fix: execute ui tests on ut-test

--- bin/ut-test.js
+++ bin/ut-test.js
@@ -5,3 +5,9 @@ require('../lib/exec')('node', [
     require('../lib/babelNodePath'),
     require.resolve('blue-tape/bin/blue-tape'),
     'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(process.argv.slice(2)));
+
+try {
+    require('../lib/exec')(require.resolve('jest/bin/jest'), []);
+} catch (e) {
+    console.error('No ui tests')
+}
