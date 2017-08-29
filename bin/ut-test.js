2f23b255108a30b0598dfc0796d56aafba1b14e4
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-08-29 18:18:31 +0300

    fix: jest testMatch glob pattern

--- bin/ut-test.js
+++ bin/ut-test.js
@@ -6,6 +6,7 @@ require('../lib/exec')('node', [
     require.resolve('blue-tape/bin/blue-tape'),
     'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(process.argv.slice(2)));
 
-require('../lib/exec')(require.resolve('jest/bin/jest'), [
-    '--testMatch=\'**/__tests__/**/*.js?(x)\''
+require('../lib/exec')('node', [
+    require.resolve('jest/bin/jest'),
+    '--testMatch=**/__tests__/**/*.js?(x)'
 ]);
