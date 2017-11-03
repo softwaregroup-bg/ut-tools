c51f6d738e969748aa07c7f43131c184f06c1d5b
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-03 08:30:12 +0200

    feat: parallel coverage

---
+++ bin/ut-cover-parallel.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+/* eslint no-process-env:0 */
+
+require('../lib/exec')('node', [
+    require('../lib/babelNodePath'),
+    'test/integration/db'].concat(process.argv.slice(2)));
+
+require('../lib/exec')('node', [
+    require('../lib/babelNodePath'),
+    require.resolve('tap/bin/run'),
+    '--cov',
+    '-J',
+    '-T',
+    'test/integration'].concat(process.argv.slice(2)));
+
+require('../lib/exec')('node', [
+    require.resolve('jest/bin/jest'),
+    '--testMatch=**/__tests__/**/*.js?(x)',
+    '--maxWorkers=2'
+]);
