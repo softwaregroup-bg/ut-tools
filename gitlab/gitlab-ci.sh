57c0f6ef1c783f76afaaca7be453439a7475ca22
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-06 15:12:19 +0200

    fix: change URL

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -5,17 +5,17 @@ then
   echo building ${BASH_REMATCH[1]}
   if [ -f yarnPrefetch.json ]
   then
-      wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci-yarn.dockerfile -O gitlab-ci.dockerfile && \
+      wget https://raw.githubusercontent.com/softwaregroup-bg/ut-tools/master/gitlab/gitlab-ci-yarn.dockerfile -O gitlab-ci.dockerfile && \
       docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
       docker run -i --rm -v /srv/npm:/root/.npm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   else
     if [ -f prefetch.json ]
     then
-      wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci-prefetch.dockerfile -O gitlab-ci.dockerfile && \
+      wget https://raw.githubusercontent.com/softwaregroup-bg/ut-tools/master/gitlab/gitlab-ci-prefetch.dockerfile -O gitlab-ci.dockerfile && \
       docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
       docker run -i --rm -v /srv/npm:/root/.npm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
     else
-      wget https://git.softwaregroup-bg.com/ut5/ut-tools/raw/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
+      wget https://raw.githubusercontent.com/softwaregroup-bg/ut-tools/master/gitlab/gitlab-ci.dockerfile -O gitlab-ci.dockerfile && \
       docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
       docker run -i --rm -v /srv/npm:/root/.npm -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
     fi
