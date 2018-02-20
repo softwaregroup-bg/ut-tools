4ea40b524e8e30ba6ae5bd790851012576c3c930
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2018-02-20 14:30:45 +1000

    fix: use prerelease URL

--- gitlab/gitlab-ci.sh
+++ gitlab/gitlab-ci.sh
@@ -5,7 +5,7 @@ then
   echo building ${BASH_REMATCH[1]}
   if [ -f yarnPrefetch.json ]
   then
-      wget https://raw.githubusercontent.com/softwaregroup-bg/ut-tools/master/gitlab/gitlab-ci-yarn.dockerfile -O gitlab-ci.dockerfile && \
+      wget https://raw.githubusercontent.com/softwaregroup-bg/ut-tools/major/rc-cubalibre/gitlab/gitlab-ci-yarn.dockerfile -O gitlab-ci.dockerfile && \
       docker build -t ${BASH_REMATCH[1]} -f gitlab-ci.dockerfile . && \
       docker run -i --rm -v /srv/yarn:/usr/local/share/.cache/yarn -e TAP_COLORS -e TAP_TIMEOUT -e CI_BUILD_ID -e UT_ENV -e UT_DB_PASS -e UT_MODULE=${BASH_REMATCH[5]} ${BASH_REMATCH[1]} npm run gitlab
   else
