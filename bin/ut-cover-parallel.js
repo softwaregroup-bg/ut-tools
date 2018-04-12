09a31f1b1be77d03f6848057f24b82da364045d7
elin.angelov <elin.angelov@softwaregroup-bg.com>
2018-04-12 13:30:46 +0300

    fix: cover-paralel unit tests

--- bin/ut-cover-parallel.js
+++ bin/ut-cover-parallel.js
@@ -8,6 +8,12 @@ require('../lib/exec')(process.execPath, [
     'test/integration'].concat(process.argv.slice(2)));
 
 require('../lib/exec')(process.execPath, [
+    require.resolve('tap/bin/run'),
+    '--cov',
+    '-j4',
+    'test/unit'].concat(process.argv.slice(2)));
+
+require('../lib/exec')(process.execPath, [
     require.resolve('jest/bin/jest'),
     '--testMatch=**/__tests__/**/*.js?(x)',
     '--maxWorkers=2'
