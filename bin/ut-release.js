be7267c2e8639bd1ed7ff8df7c951e667e6ae311
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2018-12-06 12:08:54 +1000

    fix: remove obfuscator, as it is never used

--- bin/ut-release.js
+++ bin/ut-release.js
@@ -2,12 +2,10 @@
 /* eslint no-console:0, no-process-exit:0 */
 const exec = require('../lib/exec');
 const versionBump = require('../lib/versionBump');
-const obfuscator = require('../lib/obfuscator');
 versionBump()
     .then(({tag}) => {
         exec('git', ['push']);
         exec('git', ['push', 'origin', '--tags']);
-        obfuscator.obfuscate();
         return exec('npm', tag ? ['publish', '--tag', tag] : ['publish']);
     })
     .catch(function(e) {
