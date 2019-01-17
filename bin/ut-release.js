733b41b93f1b0cce7a87e70fc7cf1245ed380fe3
Kalin Krustev <kalin.krustev@gmail.com>
2019-01-17 08:44:02 +0200

    feat: add result on release

--- bin/ut-release.js
+++ bin/ut-release.js
@@ -2,12 +2,15 @@
 /* eslint no-console:0, no-process-exit:0 */
 const exec = require('../lib/exec');
 const versionBump = require('../lib/versionBump');
+const fs = require('fs');
+
 versionBump()
     .then(({tag}) => {
         exec('git', ['push']);
         exec('git', ['push', 'origin', '--tags']);
         return exec('npm', tag ? ['publish', '--tag', tag] : ['publish']);
     })
+    .then(() => fs.copyFileSync && fs.copyFileSync('package.json', 'result.json'))
     .catch(function(e) {
         console.error(e);
         process.exit(1);
