b51f67e3234c1086dd942c7fdbb197a0e95ab45b
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-02-04 10:15:27 +1000

    feat: add *.test.js to tests

--- bin/ut-cover-parallel.js
+++ bin/ut-cover-parallel.js
@@ -7,6 +7,7 @@ require('../lib/exec')(process.execPath, [
     '--coverage-report=text',
     '-j' + (process.env.TAP_JOBS || '8'),
     'test/integration',
+    '**/*.test.js',
     'test/unit/cases'].concat(process.argv.slice(2)));
 
 require('../lib/exec')(process.execPath, [
