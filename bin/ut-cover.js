001a1ff323403cf686b3ce775531692087589f36
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-02-09 08:49:32 +1000

    fix: remove unneeded code

--- bin/ut-cover.js
+++ bin/ut-cover.js
@@ -1,37 +1,3 @@
 #!/usr/bin/env node
-/* eslint no-process-env:0 */
-var fs = require('fs');
 
-require('../lib/exec')(require.resolve('nyc/bin/nyc'), [
-    '--reporter=lcov',
-    '--reporter=text',
-    '--reporter=cobertura',
-    '--include=**/*.js',
-    '--exclude=**/ut-tools/**/*.js',
-    '--exclude=!node_modules/ut-*/**/*.js',
-    '--exclude=node_modules/!(ut-*)/**/*.js',
-    '--exclude=**/*marko.js',
-    '--exclude=**/test/**']
-    .concat(
-        process.env.UT_COVER_DIR ? [`--report-dir=${process.env.UT_COVER_DIR}`] : []
-    )
-    .concat([
-        'node',
-        require('../lib/babelNodePath'),
-        require.resolve('blue-tape/bin/blue-tape'),
-        'test/**/start*.js',
-        'test/**/test*.js',
-        'test/**/stop*.js'
-    ]));
-require('../lib/exec')('node', [
-    require.resolve('jest/bin/jest'),
-    '--coverage',
-    '--passWithNoTests',
-    '--testMatch=**/__tests__/**/*.js?(x)',
-    '--maxWorkers=2']
-    .concat(
-        process.env.UT_COVER_DIR ? [`--coverageDirectory=${process.env.UT_COVER_DIR}_ui`] : []
-    ));
-if (process.env.UT_COVER_DIR) {
-    fs.appendFileSync(`${process.env.UT_COVER_DIR}/lcov.info`, fs.readFileSync(`${process.env.UT_COVER_DIR}_ui/lcov.info`));
-}
+require('./ut-cover-parallel');
--- bin/ut-test.js
+++ bin/ut-test.js
@@ -1,14 +1,3 @@
 #!/usr/bin/env node
-/* eslint no-process-env:0 */
 
-require('../lib/exec')('node', [
-    require('../lib/babelNodePath'),
-    require.resolve('blue-tape/bin/blue-tape'),
-    'test/**/start*.js', 'test/**/test*.js', 'test/**/stop*.js'].concat(process.argv.slice(2)));
-
-require('../lib/exec')('node', [
-    require.resolve('jest/bin/jest'),
-    '--testMatch=**/__tests__/**/*.js?(x)',
-    '--passWithNoTests',
-    '--maxWorkers=2'
-]);
+require('./ut-test-parallel');
