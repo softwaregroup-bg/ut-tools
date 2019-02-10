ab035839410e0c6c892e79835551dbb30b5bd423
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-02-10 12:41:00 +1000

    feat: more test formats

--- bin/ut-test-parallel.js
+++ bin/ut-test-parallel.js
@@ -10,6 +10,20 @@ require('../lib/exec')(process.execPath, [
     '!(node_modules)/**/*.test.js',
     'test/unit/cases'].concat(process.argv.slice(2)));
 
+require('../lib/exec')('"' + process.execPath + '"', [
+    require.resolve('tap-mocha-reporter'),
+    'xunit',
+    '<.lint/tap.txt',
+    '>.lint/xunit.xml'
+], {shell: true});
+
+require('../lib/exec')('"' + process.execPath + '"', [
+    require.resolve('tap-mocha-reporter'),
+    'classic',
+    '<.lint/tap.txt',
+    '>.lint/test.txt'
+], {shell: true});
+
 require('../lib/exec')(process.execPath, [
     require.resolve('jest/bin/jest'),
     '--testMatch=**/__tests__/**/*.js?(x)',
