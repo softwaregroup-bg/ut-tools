807d91fd0a3ecef0c1475f7cd95a1bcae2b25370
Kalin Krustev <kalin.krustev@gmail.com>
2019-03-20 10:28:58 +0200

    feat: allow compile during release

--- bin/ut-release.js
+++ bin/ut-release.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/* eslint no-console:0, no-process-exit:0 */
+/* eslint no-console:0, no-process-exit:0, no-process-env:0 */
 const exec = require('../lib/exec');
 const versionBump = require('../lib/versionBump');
 const fs = require('fs');
@@ -8,6 +8,7 @@ versionBump()
     .then(({tag}) => {
         exec('git', ['push']);
         exec('git', ['push', 'origin', '--tags']);
+        if (process.env.npm_package_scripts_compile) exec('npm', ['run', 'compile']);
         return exec('npm', tag ? ['publish', '--tag', tag] : ['publish']);
     })
     .then(() => fs.copyFileSync && fs.copyFileSync('package.json', '.lint/result.json'))
