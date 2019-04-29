2319e253bd64659e54e6ebc81a67bda6d5ab3d84
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-04-29 21:35:04 +1000

    fix: test existing paths only

--- bin/ut-cover-parallel.js
+++ bin/ut-cover-parallel.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 /* eslint no-process-env:0, no-process-exit:0 */
+const fs = require('fs');
+var paths = [];
+if (fs.existsSync('test/integration')) paths.push('test/integration');
+if (fs.existsSync('test/unit/cases')) paths.push('test/unit/cases');
 
 let test = require('../lib/exec')('"' + process.execPath + '"', [
     require.resolve('tap/bin/run'),
@@ -8,9 +12,7 @@ let test = require('../lib/exec')('"' + process.execPath + '"', [
     '--output-file=.lint/tap.txt',
     '--reporter=classic',
     '-j' + (process.env.TAP_JOBS || '8'),
-    'test/integration',
-    '"!(node_modules|tap-snapshots)/**/*.test.js"',
-    'test/unit/cases'].concat(process.argv.slice(2)), {shell: true}, false);
+    '"!(node_modules|tap-snapshots)/**/*.test.js"'].concat(paths, process.argv.slice(2)), {shell: true}, false);
 
 require('../lib/exec')('"' + process.execPath + '"', [
     require.resolve('tap/bin/run'),
@@ -38,10 +40,3 @@ require('../lib/exec')('"' + process.execPath + '"', [
 ], {shell: true}, false);
 
 if (test === false) process.exit(1);
-
-require('../lib/exec')('"' + process.execPath + '"', [
-    require.resolve('jest/bin/jest'),
-    '"--testMatch=**/__tests__/**/*.js?(x)"',
-    '--passWithNoTests',
-    '--maxWorkers=2'
-], {shell: true});
--- bin/ut-test-parallel.js
+++ bin/ut-test-parallel.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 /* eslint no-process-env:0, no-process-exit:0 */
+const fs = require('fs');
+var paths = [];
+if (fs.existsSync('test/integration')) paths.push('test/integration');
+if (fs.existsSync('test/unit/cases')) paths.push('test/unit/cases');
 
 let test = require('../lib/exec')('"' + process.execPath + '"', [
     require.resolve('tap/bin/run'),
     '--output-file=.lint/tap.txt',
     '--reporter=classic',
     '-j' + (process.env.TAP_JOBS || '8'),
-    'test/integration',
-    '"!(node_modules|tap-snapshots)/**/*.test.js"',
-    'test/unit/cases'].concat(process.argv.slice(2)), {shell: true}, false);
+    '"!(node_modules|tap-snapshots)/**/*.test.js"'].concat(paths, process.argv.slice(2)), {shell: true}, false);
 
 require('../lib/exec')('"' + process.execPath + '"', [
     require.resolve('tap-mocha-reporter'),
@@ -25,10 +27,3 @@ require('../lib/exec')('"' + process.execPath + '"', [
 ], {shell: true}, false);
 
 if (test === false) process.exit(1);
-
-require('../lib/exec')('"' + process.execPath + '"', [
-    require.resolve('jest/bin/jest'),
-    '"--testMatch=**/__tests__/**/*.js?(x)"',
-    '--passWithNoTests',
-    '--maxWorkers=2'
-], {shell: true});
