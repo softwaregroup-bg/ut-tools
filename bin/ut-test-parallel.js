4a5b9f353eddee0092645d6f534e3bbb62031fb7
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-02-06 08:42:21 +1100

    fix: resolve direct devDependency first

--- bin/ut-test-parallel.js
+++ bin/ut-test-parallel.js
@@ -2,19 +2,20 @@
 /* eslint no-process-env:0, no-process-exit:0 */
 const fs = require('fs');
 const {testFiles} = require('../lib/defaults');
+const resolve = require('resolve');
 
 var paths = [];
 if (fs.existsSync('test/integration')) paths.push('test/integration');
 if (fs.existsSync('test/unit/cases')) paths.push('test/unit/cases');
 
 const test = require('../lib/exec')('"' + process.execPath + '"', [
-    require.resolve('tap/bin/run'),
+    resolve.sync('tap/bin/run', {basedir: '.'}),
     '--output-file=.lint/tap.txt',
     '--reporter=classic',
     '-j' + (process.env.TAP_JOBS || '8')].concat(testFiles, paths, process.argv.slice(2)), {shell: true}, false);
 
 require('../lib/exec')('"' + process.execPath + '"', [
-    require.resolve('tap-mocha-reporter'),
+    resolve.sync('tap/bin/run', {basedir: '.'}),
     'xunit',
     '<.lint/tap.txt',
     '>.lint/xunit.xml'
