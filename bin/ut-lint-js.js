c6b6b6cce9773ee13c5d34df827fe3f27acbdffb
Kalin Krustev <kalin.krustev@softwaregroup.com>
2020-11-14 22:35:27 +0200

    fix: typo

--- bin/ut-lint-js.js
+++ bin/ut-lint-js.js
@@ -2,7 +2,7 @@
 /* eslint no-process-exit:0 */
 (async() => {
     try {
-        if (!await require('../lib/lintJS')()) process.exit(1);
+        if (await (require('../lib/lintJS')())) process.exit(1);
     } catch (error) {
         // eslint-disable-next-line no-console
         console.error(error);
