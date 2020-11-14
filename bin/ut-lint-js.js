a197c4fef7564d45eaaf867a0adba833998aa78a
Kalin Krustev <kalin.krustev@softwaregroup.com>
2020-11-14 21:45:03 +0200

    fix: await async lint

--- bin/ut-lint-js.js
+++ bin/ut-lint-js.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 /* eslint no-process-exit:0 */
-require('../lib/lintJS')() && process.exit(1);
+(async() => {
+    try {
+        if (!await require('../lib/lintJS')()) process.exit(1);
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        process.exit(1);
+    }
+})();
