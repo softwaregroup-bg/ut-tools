87303b7842fc4c2bc2513bf9be121346e287b6c4
Kalin Krustev <kalin.krustev@gmail.com>
2022-06-13 00:32:16 +0300

    fix: global usage

--- bin/ut-changelog.js
+++ bin/ut-changelog.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-require('../lib/exec')('conventional-changelog', ['-p', 'angular', '-i', 'CHANGELOG.md', '-s', '-c', require.resolve('../context.js')]);
+require('../lib/exec')(require.resolve('conventional-changelog-cli/cli'), ['-p', 'angular', '-i', 'CHANGELOG.md', '-s', '-c', require.resolve('../context.js')]);
 require('../lib/exec')('git', ['add', 'CHANGELOG.md']);
