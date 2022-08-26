2e56a65c4505971bc445368cc1c501e4a7438bb4
Stamen Peev <stamen.peev@softwaregroup-bg.com>
2022-08-26 15:54:29 +0300

    fix: pass arguments to npm run doc

--- bin/ut-release.js
+++ bin/ut-release.js
@@ -10,7 +10,16 @@ versionBump()
     .then(({tag}) => {
         exec('git', ['push']);
         exec('git', ['push', 'origin', '--tags']);
-        if (require(process.env.npm_package_json)?.scripts?.doc) exec('npm', ['run', 'doc']);
+        if (require(process.env.npm_package_json)?.scripts?.doc) {
+            exec('npm', ['run', 'doc',
+                '--fromVersion', process.env.npm_package_json.version,
+                '--toolsUrl', process.env.TOOLS_URL,
+                '--toolsUsername', process.env.TOOLS_USERNAME,
+                '--toolsPassword', process.env.TOOLS_PASSWORD,
+                '--branchName', process.env.BRANCH_NAME,
+                '--buildNumber', process.env.BUILD_NUMBER
+            ]);
+        }
         if (require(process.env.npm_package_json)?.scripts?.compile) exec('npm', ['run', 'compile']);
         return exec('npm', (tag ? ['publish', '--tag', tag] : ['publish']).concat(process.argv.slice(2)));
     })
