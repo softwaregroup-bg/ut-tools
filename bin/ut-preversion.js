48fb8f5890ad7e248fac7f661558f0c24f935f86
Stamen Peev <stamen.peev@softwaregroup-bg.com>
2024-06-13 14:28:52 +0300

    chore: linting

--- bin/ut-preversion.js
+++ bin/ut-preversion.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint no-process-env:0 */
 const exec = require('../lib/exec');
 exec('npm', ['run', 'cover']);
 if (process.env.npm_package_json && require(process.env.npm_package_json).scripts?.license) {
